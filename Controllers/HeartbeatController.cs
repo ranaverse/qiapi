34906e5674c2cf563aa238365c4467bf9fb73854
ranaverse <ranaqi@proton.me>
2025-06-21 09:40:51 +0200

    🧹 Clean structure and prepare for sync

---
+++ Controllers/HeartbeatController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using qiapi.Models;
+
+namespace qiapi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HeartbeatController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var response = new Heartbeat
+            {
+                Message = "Heartbeat received. The oracle is alive.",
+                Timestamp = DateTime.UtcNow,
+                Source = "QI Core"
+            };
+
+            return Ok(response);
+        }
+    }
+
+}
---
+++ Models/Heartbeat.cs
@@ -0,0 +1,9 @@
+﻿namespace qiapi.Models
+{
+    public class Heartbeat
+    {
+        public string Message { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Source { get; set; }
+    }
+}
